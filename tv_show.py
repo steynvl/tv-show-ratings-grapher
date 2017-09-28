0979b4e48e4e9226287454bd5114cab86a709351
steynvl <steyn.van.l@gmail.com>
2017-09-28 20:46:15 +0200

    Building of query working

---
+++ tv_show.py
@@ -0,0 +1,19 @@
+import sys
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import quote
+
+def _build_query(name):
+    return "http://www.imdb.com/find?ref_=nv_sr_fn&q={}&s=tt".format(quote(name).replace("%20", "+"))
+
+def main():
+    query = _build_query(sys.argv[1])
+    soup = BeautifulSoup(requests.get(query).text, 'lxml')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("One command line argument required... exiting")
+        sys.exit(1)
+    else:
+        sys.exit(main())
