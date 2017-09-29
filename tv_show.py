184557a2d86ca125887bd21a910de7dab6a46d98
steynvl <steyn.van.l@gmail.com>
2017-09-29 17:28:44 +0200

    Added function to webscrape episodes
    Places the data in a named tuple

--- tv_show.py
+++ tv_show.py
@@ -4,12 +4,26 @@ import sys
 from bs4 import BeautifulSoup
 import requests
 from urllib.parse import quote
+from collections import namedtuple
 
-def _build_query(name):
+def get_episodes(show_id):
+
+    Episode = namedtuple('Episode', 'ep name rating votes')
+
+    soup = BeautifulSoup(requests.get('http://www.imdb.com/title/{}/epdate'.format(show_id)).text, 'lxml')
+
+    info = soup.find(id='tn15content')
+    if len(info.contents) <= 1:
+        yield
+    else:
+        for row in info.table('tr')[1:]:
+            yield Episode(*[td.get_text(strip=True) for td in row('td')[:4]])
+
+def build_query(name):
     return "http://www.imdb.com/find?ref_=nv_sr_fn&q={}&s=tt".format(quote(name).replace("%20", "+"))
 
 def main():
-    query = _build_query(sys.argv[1])
+    query = build_query(sys.argv[1])
     soup = BeautifulSoup(requests.get(query).text, 'lxml')
 
 
